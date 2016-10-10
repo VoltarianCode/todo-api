eafedb09e935014d4a8a4c4a8016c94da10a6677
VoltarianCode <illya.semenkov@gmail.com>
2016-10-10 02:51:07 -0400

    add /todos and /todos/:id

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var todos = [{
 	id: 2,
 	description: 'Remember to eat, humans cannot photosynthesize',
 	completed: false
+}, {
+	id:3,
+	description: 'breathe',
+	completed: true
 }];
 
 app.get('/', function(req, res){
@@ -17,6 +21,33 @@ app.get('/', function(req, res){
 
 app.get('/todos', function(req, res){
 	res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res){
+
+	/*
+	if (todos[Number(req.params.id) - 1]){
+		res.json(todos[Number(req.params.id) - 1]);
+	} else {
+		res.status(404).send();
+	}
+	*/
+
+	var todoId = parseInt(req.params.id);
+	var matchedTodo;
+	todos.forEach(function(todo){
+		if (todoId === todo.id){
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo){
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+
+
 });
 
 
