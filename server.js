60d640e86070ee4c81f1769a28ac5038f2db7901
VoltarianCode <illya.semenkov@gmail.com>
2016-10-11 00:34:52 -0400

    fixed delete to use the db

--- server.js
+++ server.js
@@ -73,21 +73,22 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted){
+		if (rowsDeleted === 0){
+			res.status(404).json({
+				"error": "no todo with that id"
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function(e){
+		res.status(500).send();
 	});
-
-	if (!matchedTodo) {
-		return res.status(404).json({
-			"error": "todo with that id not found"
-		});
-	}
-	todos = _.without(todos, matchedTodo);
-
-	console.log(todos);
-
-	res.json(todos);
-
+	
 
 });
 
