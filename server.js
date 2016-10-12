dcc4674e0d9d4432048fb0220cbff573220a45eb
VoltarianCode <illya.semenkov@gmail.com>
2016-10-11 21:33:46 -0400

    DELETE for /users/login to logout users

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var db = require('./db.js');
 
 var middleware = require('./middleware.js')(db);
 
-
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -160,18 +158,34 @@ app.post('/users', function(req, res) {
 
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
+
 	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
+		userInstance = user;
+		return db.token.create({token: token});
+		/*
 		if (token) {
 			res.header('Auth', token).json(user.toPublicJSON());
 		} else {
 			res.status(401).send();
 		}
+		*/
 
-	}, function(e) {
+	}).then(function(tokenInstance){
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function(e) {
 		res.status(401).send();
 	});
 
+});
+
+app.delete('/users/login', middleware.requireAuthentication, function (req, res){
+	req.token.destroy().then(function(){
+		res.status(204).send();
+	}).catch(function(){
+		res.status(500).send();
+	});
 });
 
 
