9cebfda992e0b7d6c092b9409f84421d0841e245
VoltarianCode <illya.semenkov@gmail.com>
2016-10-11 20:16:42 -0400

    CRUD requests now only work for current logged in user

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ app.get('/', function(req, res) {
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
 
-	var where = {};
+	var where = {
+		userId: req.user.get('id')
+	};
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
 	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
@@ -48,7 +50,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 	var todoId = parseInt(req.params.id);
-	var matchedTodo = db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -68,7 +75,7 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	db.todo.create(body).then(function(todo) {
 		req.user.addTodo(todo).then(function() {
 			return todo.reload();
-		}).then(function (todo) {
+		}).then(function(todo) {
 			res.json(todo.toJSON());
 		});
 	}, function(e) {
@@ -82,7 +89,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id);
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -115,7 +123,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	}
 
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
@@ -163,9 +176,7 @@ app.post('/users/login', function(req, res) {
 
 
 
-db.sequelize.sync({
-	force: true
-}).then(function() {
+db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('Express server started on port ' + PORT);
 	});
